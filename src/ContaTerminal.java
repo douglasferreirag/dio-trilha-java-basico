f59d1b713bbd42a1e642ed61d8763f2c1ec6c267
douglasferreirag <douglas.ferreirag@gmail.com>
2024-04-24 15:40:05 -0300

    Tarefas a serem realizadas

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,19 @@
+
+
 public class ContaTerminal {
+
+
     public static void main(String[] args) throws Exception {
+
+   
+        
+       
+        //TODO: Exibir as mensagens para o nosso usuário.
+
+        //TODO: Obter pela classe Scanner os valores digitados no terminal
+
+        //TODO: Exibir a mensagem a mensagem da conta criada.
+
         System.out.println("Hello, World!");
     }
 }
